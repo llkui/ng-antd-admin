2b6ffe2fcdd9716ddb85dba6dbfa04ec3cc6062c
lliukui <liukui178@163.com>
2020-10-28 16:42:07 +0800

    sky-admin

---
+++ README.md
@@ -0,0 +1,19 @@
+# 介绍
+项目基于`angular` `ng-zorro-antd`实现的angular风格的后台管理系统
+
+# 安装
+```bash
+$ git clone https://github.com/llkui/sky-admin.git sky-admin
+$ cd sky-admin
+$ npm install
+```
+
+# 开发环境
+```bash
+$ ng serve --open
+```
+
+# 打包
+```bash
+$ ng build --prod
+```
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
