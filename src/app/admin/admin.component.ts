5b23d68d6109390decfae63bff9468b025410793
llkui <llkui241569@163.com>
2020-11-30 15:16:30 +0800

    add header

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlatformLocation } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
     selector: 'app-admin',
@@ -9,9 +12,23 @@ import { PlatformLocation } from '@angular/common';
 
 export class AdminComponent implements OnInit {
     menuList = [];
+    showSearch = false;
+    searchOptions = [];
+    inputValue = 'Pro Table';
+    noticeList = [];
+    messageList = [];
+    taskList = [];
+    bellInfo = {
+        total: 11,
+        notice: 4,
+        message: 3,
+        task: 4,
+    };
 
     constructor(
-        private platformLocation: PlatformLocation
+        private platformLocation: PlatformLocation,
+        private router: Router,
+        private message: NzMessageService
     ) { }
 
     ngOnInit() {
@@ -172,5 +189,81 @@ export class AdminComponent implements OnInit {
          * 1、PathLocationStrategy：const url = this.platformLocation.pathname
          * 2、HashLocationStrategy：const url = this.platformLocation.hash.slice(1);
          */
+
+        this.searchOptions = ['umi ui', 'Ant Design', 'Pro Table', 'Pro Layout'];
+
+        this.noticeList = [
+            { id: 1, type: 1, title: '你收到了 14 份新周报', time: '3 年前', read: false },
+            { id: 2, type: 2, title: '你推荐的 曲妮妮 已通过第三轮面试', time: '3 年前', read: false },
+            { id: 3, type: 3, title: '这种模板可以区分多种通知类型', time: '3 年前', read: true },
+            { id: 4, type: 4, title: '左侧图标用于区分不同的类型', time: '3 年前', read: false },
+            { id: 5, type: 1, title: '内容不要超过两行字，超出时自动截断', time: '3 年前', read: false },
+        ];
+
+        this.messageList = [
+            {
+                id: 1, avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+                title: '曲丽丽 评论了你', desc: '描述信息描述信息描述信息', time: '3 年前', read: false
+            },
+            {
+                id: 2, avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+                title: '朱偏右 回复了你', desc: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像', time: '3 年前', read: false
+            },
+            {
+                id: 3, avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+                title: '标题', desc: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像', time: '3 年前', read: false
+            },
+        ];
+
+        this.taskList = [
+            { id: 1, type: 1, title: '任务名称', desc: '任务需要在 2017-01-12 20:00 前启动', read: false },
+            { id: 2, type: 2, title: '第三方紧急代码变更', desc: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务', read: false },
+            { id: 3, type: 3, title: '信息安全考试', desc: '指派竹尔于 2017-01-09 前完成更新并发布', read: false },
+            { id: 4, type: 4, title: 'ABCD 版本发布', desc: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务', read: false },
+        ];
+    }
+
+    search() {
+        this.showSearch = true;
+    }
+
+    inputBlur() {
+        this.showSearch = false;
+    }
+
+    goUrl(url) {
+        this.router.navigate([url]);
+    }
+
+    read(item) {
+        item.read = true;
+        this.getBellInfo();
+    }
+
+    readAll(type) {
+        if (type === 'notice') {
+            this.noticeList = [];
+        } else if (type === 'message') {
+            this.messageList = [];
+        } else {
+            this.taskList = [];
+        }
+        this.getBellInfo();
+    }
+
+    getBellInfo() {
+        const noticeLength = this.noticeList.filter(i => !i.read).length;
+        const messageLength = this.messageList.filter(i => !i.read).length;
+        const taskLength = this.taskList.filter(i => !i.read).length;
+        this.bellInfo = {
+            total: noticeLength + messageLength + taskLength,
+            notice: noticeLength,
+            message: messageLength,
+            task: taskLength
+        };
+    }
+
+    readMore(type) {
+        this.message.success('Click on view more');
     }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 import { AuthGuard } from './../service/auth-guard.service';
 
@@ -28,11 +40,23 @@ import { ChangelogComponent } from './changelog/changelog.component';
     ],
     imports: [
         CommonModule,
+        FormsModule,
         NzLayoutModule,
         NzMenuModule,
         NzIconModule,
         NzCardModule,
         NzTimelineModule,
+        NzInputModule,
+        NzAutocompleteModule,
+        NzToolTipModule,
+        NzBadgeModule,
+        NzAvatarModule,
+        NzDropDownModule,
+        NzTabsModule,
+        NzListModule,
+        NzEmptyModule,
+        NzMessageModule,
+        NzTagModule,
         AdminRoutingModule
     ],
     providers: [
